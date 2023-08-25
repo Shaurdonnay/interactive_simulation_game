a984f6a7f02452488f32385bbd4d7662ace3beee
Shaurdonnay <79025532+Shaurdonnay@users.noreply.github.com>
2023-08-25 14:38:17 -0600

    Add files via upload

---
+++ ProjectInteractiveGameRefactoring.h
@@ -0,0 +1,18 @@
+void callGame() {
+    std::cout << "=========================================\n";
+    std::cout << "===============Abduction=================\n";
+    std::cout << "===Interactive Game By Natasha Johnson===\n";
+    std::cout << "=========================================\n\n\n";
+}
+void questionOne() {
+    std::cout << "Its night, and you are walking in the woods... You see a strange flying object coming towards you, what do you do?\n";
+}
+void questionTwo() {
+    std::cout <<"You see that it is a spaceship, and now it has started to chase you. What do you do?\n";
+}
+void questionThree() {
+    std::cout<< "You see that it is for sure a spaceship, and its getting closer. What do you do?\n";
+}
+void questionFour() {
+    std::cout <<"The spaceship lands right in front of you. What do you do?\n";
+}
